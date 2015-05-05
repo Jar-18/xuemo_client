f1fb76210077478dbb65c72dd10f5fd4a82d2aa1
amberonno <amberonno@gmail.com>
2015-05-06 01:17:30 +0800

    开发筛选条

--- xuemo/www/module/app/learn/index.js
+++ xuemo/www/module/app/learn/index.js
@@ -10,23 +10,29 @@ angular.module('starter.controllers')
         },{
             level1:routerParams.sort
         }];
+        $scope.$watch("activeFilterList",function(newVal,oldVal){
+            if(newVal && newVal[0].level1==oldVal[0].level1){
+                $scope.refreshCourseList();
+            }
+        },true);
         $scope.courseList=[];
         $scope.noMoreItemsAvailable = false;
         $scope.pageNumber=1;
         $scope.pageSize=10;
         $scope.refreshCourseList=function(){
+            $scope.pageNumber=1;
             var goodsListPromise=courseService.getCoursePromise(undefined,{
                 pageNumber:$scope.pageNumber,
-                pageSize:$scope.pageSize
+                pageSize:$scope.pageSize,
+                categoryId:$scope.activeFilterList[1].level1,
+                districtId:$scope.activeFilterList[1].level2
             });
             goodsListPromise.success(function(data,status,headers){
                 var courseList=data;
                 if(courseList.length==0){
                     $scope.noMoreItemsAvailable = true;
                 }
-                for(var goodsIndex= 0;goodsIndex<courseList.length;goodsIndex++){
-                    $scope.courseList.push(courseList[goodsIndex]);
-                };
+                $scope.courseList=courseList;
                 $scope.pageNumber++;
             }).finally(function(){
                 $scope.$broadcast('scroll.infiniteScrollComplete');
