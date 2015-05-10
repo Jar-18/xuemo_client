d013d634d33dbd66f26635e7569bcd72a26ab12e
amberonno <amberonno@gmail.com>
2015-05-10 10:43:03 +0800

    更新对应的url规则

--- xuemo/www/module/app/tabs/index.js
+++ xuemo/www/module/app/tabs/index.js
@@ -1,19 +1,6 @@
 angular.module('starter.controllers')
 
     .controller('mainTabsCtrl', ['$scope','$ionicHistory','$state','userService',function($scope,$ionicHistory,$state,userService) {
-        $scope.showTabs=true;
-        $scope.$root.$on('$stateChangeStart',
-            function(event, toState, toParams, fromState, fromParams){
-                var currentState= toState.name;
-                var showTabs=false;
-                var level1TabList=['app.learn','app.teach','app.find','app.news','app.me'];
-                for(var index=0;index<level1TabList.length;index++){
-                    if(currentState==level1TabList[index]){
-                        showTabs=true;
-                        break;
-                    }
-                }
-            });
         $scope.navTo=function(state){
             $scope.currentStatus=state;
             $ionicHistory.nextViewOptions({
